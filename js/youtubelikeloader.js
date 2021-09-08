ba8153301f05ceb6ed701723ca14f5b0882c3188
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-09-08 13:42:44 +0530

    Update youtubelikeloader.js

--- js/youtubelikeloader.js
+++ js/youtubelikeloader.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 $('.bar1').click(function() {
 $('.bar').each(function() {
 $('.yt').each(function() {
-$('.bar').css('width','90%');
-$('.yt').css('width','90%',);
+$('.bar').css('width','98%');
+$('.yt').css('width','98%',);
 $('.bar').on('transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd', function(event) { 
 $('.bar').css('display','none');
 $('.yt').css('display','none');
