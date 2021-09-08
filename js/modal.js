fd1485ddf728baacedc9d381431418197a355131
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-09-09 00:55:58 +0530

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,49 +1,49 @@
 $(document).ready(function(){
-$(".myfix-1").click(f1(){
+$(".myfix-1").click(function(){
 $("#box-1").load("./data/post-1.html");
 });
 
-$(".myfix-2").click(f2(){
+$(".myfix-2").click(function(){
 $("#box-2").load("./data/post-2.html");
 });
 
-$(".myfix-3").click(f3(){
+$(".myfix-3").click(function(){
 $("#box-3").load("./data/post-3.html");
 });
 
-$(".myfix-4").click(f4(){
+$(".myfix-4").click(function(){
 $("#box-4").load("./data/post-4.html");
 });
 
-$(".myfix-5").click(f5(){
+$(".myfix-5").click(function(){
 $("#box-5").load("./data/post-5.html");
 });
 
-$(".myfix-6").click(f6(){
+$(".myfix-6").click(function(){
 $("#box-6").load("./data/post-6.html");
 });
 
-$(".myfix-7").click(f7(){
+$(".myfix-7").click(function(){
 $("#box-7").load("./data/post-7.html");
 });
 
-$(".myfix-8").click(f8(){
+$(".myfix-8").click(function(){
 $("#box-8").load("./data/post-8.html");
 });
 
-$(".myfix-9").click(f9(){
+$(".myfix-9").click(function(){
 $("#box-9").load("./data/post-9.html");
 });
 
-$(".myfix-10").click(f10(){
+$(".myfix-10").click(function(){
 $("#box-10").load("./data/post-10.html");
 });
 
-$(".myfix-11").click(f11(){
+$(".myfix-11").click(function(){
 $("#box-11").load("./data/post-11.html");
 });
 
-$(".myfix-12").click(f12(){
+$(".myfix-12").click(function(){
 $("#box-12").load("./data/post-12.html");
 });
 });
