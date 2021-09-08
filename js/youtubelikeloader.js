4ead3b848d8bd8cff1ddec32e150a8a24dd93a19
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-09-08 17:45:12 +0530

    Update youtubelikeloader.js

--- js/youtubelikeloader.js
+++ js/youtubelikeloader.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 $('.bar1').click(function() {
+$('.bar').each(function() {
 $('.bar').css('width','100%');
+$('.yt').each(function() {
 $('.yt').css('width','100%',);
 $('.bar').on('transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd', function(event) { 
 $('.bar').css('display','none');
 $('.yt').css('display','none');
+});
+});
 });
 });
 });
