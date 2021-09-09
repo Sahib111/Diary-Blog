b45ee0a687f805f309d42626ca3e945ffdcc5eea
Sahib111 <58338778+Sahib111@users.noreply.github.com>
2021-09-09 13:29:06 +0530

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $("#myfix-1").click(function(){
-$.ajax({url: "./data/post-1.html", success: function(result){
+$.ajax({url: "./data/post-1.html", async:"false", success: function(result){
 $("#box-1").html(result);
 }});
 });
@@ -8,7 +8,7 @@ $("#box-1").html(result);
 
 $(document).ready(function(){
 $("#myfix-2").click(function(){
-$.ajax({url: "./data/post-2.html", success: function(result){
+$.ajax({url: "./data/post-2.html", async:"false", success: function(result){
 $("#box-2").html(result);
 }});
 });
